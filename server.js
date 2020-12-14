5e34221405acd9ca7ef63270f4e1d538b4411347
VincenzoDeLucia <vincenzo.delucia@icloud.com>
2020-12-14 15:22:38 +0100

    About to merge with Querien

--- server.js
+++ server.js
@@ -13,24 +13,9 @@ const io = socketio(server, {
 });
 
 io.on("connection", (socket) => {
-  const myUser = JSON.parse(socket.handshake.query.myUser);
-
-  socket.join(socket.handshake.query.user);
-  const obj = Array.from(io.sockets.adapter.rooms).reduce((acc, [key, val]) => {
-    return { ...acc, [key]: val };
-  }, {});
-  console.log(obj.Querien2);
-  socket
-    .to(socket.handshake.query.user) //this is the room name
-    .emit(
-      "some event",
-      `There are ${
-        obj[socket.handshake.query.user].size
-      } people in this room, and ${myUser.username} just joined `
-    );
-
-  // console.log(`Client with id ${socket.id} connected`, socket);
-  socket.emit("user connected", { "connection happened": 12 });
+  socket.on("disconnect", () => {
+    io.emit("message", "A user disconnected");
+  });
 });
 
 // io.on(‘connection’, (socket) => {
